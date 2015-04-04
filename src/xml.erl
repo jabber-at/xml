0d8c5e1c7299c83d15ef1daf1588a21d84ebcdd9
Philipp Huebner <debalance@debian.org>
2015-04-04 16:09:51 +0200

    Imported Upstream version 0.2015.03.31

--- src/xml.erl
+++ src/xml.erl
@@ -5,7 +5,7 @@
 %%% Created : 20 Nov 2002 by Alexey Shchepin <alexey@process-one.net>
 %%%
 %%%
-%%% ejabberd, Copyright (C) 2002-2013   ProcessOne
+%%% p1_xml, Copyright (C) 2002-2015   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
@@ -62,10 +62,13 @@ start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [],
 			  []).
 
+%% Can be choosen with ./configure --disable-nif
+-ifdef(DISABLE_NIF).
+init([]) ->
+    {ok, []}.
+-else.
 %% Replace element_to_binary/1 with NIF
-%% Can be choosen with ./configure --enable-nif
--ifdef(NIF).
-
+%% Can be choosen with ./configure --disable-nif
 init([]) ->
     SOPath = filename:join(get_so_path(), "xml"),
     case catch erlang:load_nif(SOPath, 0) of
@@ -73,12 +76,6 @@ init([]) ->
         Err -> error_logger:warning_msg("unable to load xml NIF: ~p~n", [Err])
     end,
     {ok, []}.
-
--else.
-
-init([]) ->
-    {ok, []}.
-
 -endif.
 
 %%% --------------------------------------------------------
